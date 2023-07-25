4673d07b40d21782fc92dc3588e7caca3eaa04b3
ak47 <ayanamirei2@naver.com>
2023-07-25 10:45:27 +0900

    첫번째 커밋

--- list.py
+++ list.py
@@ -5,9 +5,19 @@ x = [1,6,4,3,7,2,9]
 x = [1,6,4,3,7,2,9]
 max_value = max(x)
 print(max_value)
+
+
 #최소값
+x = [1,6,4,3,7,2,9]
+min_value = min(x)
+print(min_value)
+
 
 #합계
+x1 = [1,6,4,3,7,2,9]
+hab = sum(x1)
+print(hab)
+
 
 #평균
 x = [1,6,4,3,7,2,9]
